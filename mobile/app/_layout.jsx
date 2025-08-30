cc166c355a05a1bfcd9183e64fe9ef0030c17fd7
sampathvenur <sampathkumarvenur@gmail.com>
2025-08-30 06:56:54 +0530

    Authentication

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,10 +1,14 @@
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
 import SafeScreen from "@/components/SafeScreen";
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+import { ClerkProvider } from "@clerk/clerk-expo";
 
 export default function RootLayout() {
   return (
-    <SafeScreen>
-      <Stack screenOptions={{ headerShown: false }} />
-    </SafeScreen>
+    <ClerkProvider tokenCache={tokenCache}>
+      <SafeScreen>
+        <Slot />
+      </SafeScreen>
+    </ClerkProvider>
   );
 }
