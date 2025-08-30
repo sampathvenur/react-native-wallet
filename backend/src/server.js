dba9992f56c8997f67da38912e83c7d1a5a92627
sampathvenur <sampathkumarvenur@gmail.com>
2025-08-30 11:50:51 +0530

    cron.js added

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,19 +2,31 @@ import express from "express";
 import dotenv from "dotenv";
 import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
+
 import transactionsRoute from "./routes/transactionsRoute.js";
-import cors from "cors";
+import job from "./config/cron.js";
 
 dotenv.config();
 
 const app = express();
 
+if (process.env.NODE_ENV === "production") job.start();
+
 // middleware
-app.use(cors());
 app.use(rateLimiter);
 app.use(express.json());
 
+// our custom simple middleware
+// app.use((req, res, next) => {
+//   console.log("Hey we hit a req, the method is", req.method);
+//   next();
+// });
+
 const PORT = process.env.PORT || 5001;
+
+app.get("/api/health", (req, res) => {
+    res.status(200).json({ status: "ok" });
+});
 
 app.use("/api/transactions", transactionsRoute);
 
